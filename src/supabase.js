d826a13151b43aeda0d341f51ed908ae0611f3d7
Abulfaz Mustafazade <74773667+abulfazmustafazade@users.noreply.github.com>
2026-07-01 02:16:40 +0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -14,4 +14,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: { persistSession: true, autoRefreshToken: true },
+  db: {chema: "workload",},
 });
+
+
