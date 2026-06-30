7a2dbbeb2f6b4bc608aca3f4882a5b79f604c581
Abulfaz Mustafazade <74773667+abulfazmustafazade@users.noreply.github.com>
2026-06-30 18:24:33 +0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "./",                       // istənilən host (Vercel, statik) üçün nisbi yollar
-  server: { port: 5173, open: true },
-  build: { outDir: "dist" },        // Vercel default ilə uyğundur
-});
